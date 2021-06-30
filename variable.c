25954c22e35b675833778b5fda022b66eb6aa4da
mfarzamalam <mfarzamalam@gmail.com>
2021-06-30 13:40:30 +0500

    working with variables

---
+++ variable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 4;
+    int b = 10;
+    char c = 'A';
+    float grade = 6.4;
+
+    printf("Integer is %d \n", a);
+    printf("Float is %f \n", grade);
+    printf("Character is %c \n", c);
+
+    printf("Sum of %d and %d is %d \n", a, b, a + b);
+
+    return 0;
+}
