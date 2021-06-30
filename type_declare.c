c5e409ec927ad9f928e324bd8321d855af69a12a
mfarzamalam <mfarzamalam@gmail.com>
2021-06-30 17:51:22 +0500

    type_declaration

---
+++ type_declare.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    // int a = 9, b,c;
+    // printf("%d", a);
+
+    // b = c = a;
+
+    // printf("%d", b);
+    // printf("%d", c);
+
+
+    float a = 1.1;
+    float b = a + 8.9;
+
+    printf("%f", b);
+
+    printf("\n");
+    return 0;
+}
