a47eb58cb71466390787cf2e3e6c08d6182145ea
mfarzamalam <mfarzamalam@gmail.com>
2021-06-30 16:47:04 +0500

    getting user input

---
+++ input.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter the value of a: ");
+    scanf("%d", &a);
+
+    printf("Enter the value of b: ");
+    scanf("%d", &b);
+
+    printf("The sum of %d and %d is %d", a,b,a+b);
+
+    printf("\n");
+    return 0;
+}
