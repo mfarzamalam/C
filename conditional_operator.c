dc7852b2e2aabbb12d55e36c928bf5f7e287b627
mfarzamalam <mfarzamalam@gmail.com>
2021-07-02 00:06:39 +0500

    conditional operator

--- conditional_operator.c
+++ conditional_operator.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    
+    int a ;
+    printf("Enter the value: ");
+    scanf("%d", &a);
+
+    (a<5)? printf("Less than 5\n") : printf("Greater than 5\n");
 
     return 0;
 }
